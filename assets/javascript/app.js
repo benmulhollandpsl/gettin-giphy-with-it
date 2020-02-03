9fb624f01c75619a62b8aef188e9554c7040abb1
Ben Mulholland <ben.mulholland@gmail.com>
2020-02-02 20:55:26 -0500

    attempted and stopped border, console.log check at various points, want to clean up css more in future

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,19 @@
 
     
-var topics = ["Will Smith", "Gritty", "Benjamin Franklin", "cheese steak", "Philadelphia Orchestra"];
-var topicNumber = 5;
+let topics = ["Will Smith", "Gritty", "Benjamin Franklin", "cheese steak", "Philadelphia Orchestra"];
+let topicNumber = 5;
 
 $(document).ready(function(){
 
     $("#addNew").focus();
-
+    //increment buttons of var topics
     for (let i = 0; i < topics.length; i++) {
         var newButton = $("<button>");
         newButton.addClass("gifButton btn btn-success");
         newButton.attr("type", "button");
         newButton.attr("value", topics[i])
         newButton.text(topics[i]);
-        $("#topics").append(newButton);
+        $("#topics").append(newButton); 
     }
     
     $("#add").click(function () {
@@ -35,11 +35,11 @@ $(document).ready(function(){
     });
     
 });
-
+        //using api key to search through pg-13
 $(document).on("click", ".gifButton", function () {
     var searchTerm = $(this).val();
-    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=PaOSC7zPgMYPi9RUUIY90VY7tn3Q3cJP&limit=10&rating=pg";
-    // console.log(searchTerm); //was working when doing div method now said 'Failed to load resource: the server responded with a status of 403 (Forbidden)'
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm +  "&api_key=PaOSC7zPgMYPi9RUUIY90VY7tn3Q3cJP&limit=10&rating=pg-13";
+
 
     $.ajax({
         url: url,
@@ -59,8 +59,8 @@ $(document).on("click", ".gifButton", function () {
             cardDiv.addClass("card col-md-3");
 
             newGif = $("<img>");
-            newGif.addClass("gif card-img-top");
-            newGif.attr("src", result.data[i].images.fixed_height_still.url);
+            newGif.addClass("gif card-img-top"); 
+            newGif.attr("src", result.data[i].images.fixed_height_still.url);  //looks crowded but will keep this for now
             newGif.attr("data-still", result.data[i].images.fixed_height_still.url);
             newGif.attr("data-animate", result.data[i].images.fixed_height.url);
             newGif.attr("data-state", "still");
@@ -75,7 +75,7 @@ $(document).on("click", ".gifButton", function () {
 
 
         };
-        topicNumber++;
+        topicNumber++;  
     });
 
 });
@@ -84,12 +84,12 @@ $(document).on("click", ".gif", function () {
     var state = $(this).attr("data-state");
     // console.log(state);
 
-    if (state == "still") {
+    if (state == "still") {   //movement still and animate
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");  
       } else if (state == "animate") {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
       }
-    
+
 });
